4e35f053d18764145a2b0d3cfb12693206d61e8e
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-12-14 18:25:19 -0200

    First Commit

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3V5901S\\SQLEXPRESS;Initial Catalog=LojaDB;Integrated Security=False;User ID=admin;Password=1234;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            using (var contexto = new LojaContext())
+            {
+
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine("=================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                var p1 = produtos.Last();
+                p1.Nome = "007 - O Espiao Que Me Amava";
+
+                Console.WriteLine("=================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                contexto.SaveChanges();
+
+                //Console.WriteLine("=================");
+                //produtos = contexto.Produtos.ToList();
+                //foreach (var p in produtos)
+                //{
+                //    Console.WriteLine(p);
+                //}
+            }
+        }
+    }
+}

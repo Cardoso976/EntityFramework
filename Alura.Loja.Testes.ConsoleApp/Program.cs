60e59c0f6288f084643f036897d66c826ae2d2d3
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-12-15 10:08:23 -0200

    EntityFramework.Tools

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
@@ -12,46 +6,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(String[] args)
         {
-            using (var contexto = new LojaContext())
-            {
 
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
-
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Sabão em Pó",
-                    Categoria = "Limpeza",
-                    Preco = 2.99
-                };
-
-                contexto.Produtos.Add(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine("\n\n"+entry.Entity.ToString() + " - " + entry.State);
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("=================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
         }
     }
 }

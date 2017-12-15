8dbfecf78f78204e2bfb7694de620d227d8c8375
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-12-15 11:38:14 -0200

    Relacionamento 1:N

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
@@ -6,7 +11,43 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(String[] args)
         {
+            var paoFrances = new Produto()
+            {
+                Nome = "Pão Francês",
+                PrecoUnitario = 0.4,
+                Categoria = "Padaria",
+                Unidade = "Unidade"
+            };
 
+            var compra = new Compra()
+            {
+                Quantidade = 6,
+                Produto = paoFrances
+            };
+
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                contexto.Compras.Add(compra);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+
+            }         
+
+        }
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
         }
     }
 }
